c84514270a11c3c8ab15eedce7a593b02af40b23
Eric Tilton <etilton@antioch.edu>
2022-02-10 17:59:55 -0600

    Cleaned some code

--- app/src/main/java/com/capstone2022/ResetPasswordActivity.java
+++ app/src/main/java/com/capstone2022/ResetPasswordActivity.java
@@ -1,44 +1,37 @@
 package com.capstone2022;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
-    private EditText editTextUsername2;
-    private Button verify;
-
+    private EditText resetEmail;
+    private Button verificationButton;
     FirebaseAuth Fauth;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
 
-        editTextUsername2 = findViewById(R.id.username2);
-        verify = (Button) findViewById(R.id.verify);
+        resetEmail = findViewById(R.id.resetEmail);
+        verificationButton = (Button) findViewById(R.id.verify);
 
-        String email = editTextUsername2.getText().toString().trim();
+        String email = resetEmail.getText().toString().trim();
         Fauth = FirebaseAuth.getInstance();
 
-        verify.setOnClickListener(new View.OnClickListener() {
+        verificationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = editTextUsername2.getText().toString().trim();
+                String email = resetEmail.getText().toString().trim();
                 Fauth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-
 
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
@@ -48,7 +41,6 @@ public class ResetPasswordActivity extends AppCompatActivity {
                         } else {
                             Toast.makeText(ResetPasswordActivity.this, "Invalid email" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-
                     }
                 });
             }
