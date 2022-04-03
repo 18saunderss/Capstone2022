34dc9b7003e4aac8ba1f5fce4240625d2f6b5a35
18saunderss <18saunders.skyler@gmail.com>
2022-04-02 20:25:10 -0500

    anger api no longer loading

--- app/src/main/java/com/capstone2022/HomeActivity.java
+++ app/src/main/java/com/capstone2022/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.capstone2022;
 
+import static android.app.ProgressDialog.show;
+
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -38,13 +40,6 @@ public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     final FragmentManager fragmentManager = getSupportFragmentManager();
-    RequestManager manager;
-    RandomMealAdapter adapter;
-    RecyclerView recyclerView;
-    List<String> tags = new ArrayList<>();
-    Spinner spinner;
-    SearchView searchView_home;
-    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,36 +53,7 @@ public class HomeActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new HomeFragment()).commit();
 
         bottomNavigationView.setSelectedItemId(R.id.bottom_navigation);
-        recyclerView = findViewById(R.id.recycler_random);
-        spinner = findViewById(R.id.spinner_tags);
-        searchView_home = findViewById(R.id.searchView_home);
-        //progressBar = (ProgressBar)findViewById(R.id.loader);
-        //Sprite doubleBounce = new Wave();
-        //progressBar.setIndeterminateDrawable(doubleBounce);
-        manager = new RequestManager(this);
-        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.tags,
-                R.layout.spinner_text
-        );
-        arrayAdapter.setDropDownViewResource(R.layout.spinner_inner_text);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener(spinnerSelectedListener);
-
-        searchView_home.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(HomeActivity.this, "Will be added soon!", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
-        {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item){
                 BookFragment bookFragment = new BookFragment();
@@ -99,6 +65,7 @@ public class HomeActivity extends AppCompatActivity {
                 switch(item.getItemId()){
                     case R.id.ic_home:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_container, homeFragment).commit();
+
                         break;
                     case R.id.ic_list:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_container, listFragment).commit();
@@ -120,48 +87,6 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    private final RandomAPIResponseListener listener = new RandomAPIResponseListener() {
-        @Override
-        public void didFetch(List<RandomRecipe> responses, String message) {
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL));
-            adapter = new RandomMealAdapter(HomeActivity.this, responses, customOnClickListener);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setVisibility(View.VISIBLE);
-            //progressBar.setVisibility(View.GONE);
-        }
-
-        @Override
-        public void didError(String message) {
-            recyclerView.setVisibility(View.VISIBLE);
-            //progressBar.setVisibility(View.GONE);
-            Toast.makeText(HomeActivity.this, message, Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    private final AdapterView.OnItemSelectedListener spinnerSelectedListener = new AdapterView.OnItemSelectedListener() {
-        @Override
-        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-            tags.clear();
-            tags.add(adapterView.getSelectedItem().toString().toLowerCase());
-            manager.GetRandomRecipes(listener, tags);
-            recyclerView.setVisibility(View.GONE);
-            //progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> adapterView) {
-
-        }
-    };
-
-    private final CustomOnClickListener customOnClickListener = new CustomOnClickListener() {
-        @Override
-        public void onClick(String text) {
-            startActivity(new Intent(HomeActivity.this, RecipeDetailActivity.class)
-                    .putExtra("id", text));
-        }
-    };
     //BookFragment bookFragment = new BookFragment();
     //HomeFragment homeFragment = new HomeFragment();
     //ListFragment listFragment = new ListFragment();
